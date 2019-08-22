80940d7c5804fbfec39ec29f8b4018c0e806dba7
TanyaT <tanya.yt.tan@gmail.com>
2019-08-21 19:54:23 -0700

    complete cacheSolve

--- R programming/pa2_cacheSolve.R
+++ R programming/pa2_cacheSolve.R
@@ -1,2 +1,14 @@
 ##Assignment: caching the inverse of a matrix
-#cacheSolve
+cacheSolve<-function(x,...){ #x is the list returned by makeCacheMatrix
+        inver<-x$getInver() #this should be NULL? 
+        if(!is.null(inver)){
+                message("getting cached matrix")
+                return(inver) #if inver is already in the cache, then the function will 
+                                # stop here, otherwise, proceed to solve matrix
+        }
+        data<-x$get() #this will be the matrix
+        inver<-solve(data) #assign the solved matrix to inver 
+        x$setInver(inver) #set the inver to inver 
+        inver #return inver 
+        
+}
