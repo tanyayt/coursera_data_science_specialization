1728f90c47060b78bec8c7f2cc1d7ae4313d0b18
TanyaT_homepc <tanya.yt.tan@gmail.com>
2019-08-22 20:33:44 -0700

    best function completed!

--- R programming/pa3_best.R
+++ R programming/pa3_best.R
@@ -3,10 +3,15 @@ best<-function(state,outcome){
         data<-data.frame() #data stores the full data set
         state_data<-data.frame()#state_data stores the subset for the selected state
         state_outcome_data<-data.frame() #state_outcome_data stores the subset based on state and outcome
+        chosen_state<-state #assign the state argument to chosen state for readiability
+        outcome<-tolower(outcome)#convert outcome to lower case to tolerate error
         #read full data
         data<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
+        data[,11]<-as.numeric(data[,11])
+        data[,17]<-as.numeric(data[,17])
+        data[,23]<-as.numeric(data[,23])
         #check if state is valid
-        if(!is.element(state,data$state)){
+        if(!is.element(chosen_state,data$State)){
                 message("invalid state")
                 return(NA)
         }
@@ -19,23 +24,20 @@ best<-function(state,outcome){
         #using dplyr package (uncomment the next two line to install if needed)
         ##install.packages("dplyr")
         ##library(dplr)
-        ## use filter() and select()?
-
-        #coerce the column to numeric based on the outcome
-        if (outcome=="heart attack"){
-                data[,11]<-as.numeric(data[,11])
+        ## use filter() to choose only the rows of chosen state
+        state_data<-filter(data,State==chosen_state)
+        ##arrange by the outcome measure and then return the first row
+        if(outcome=="heart attack"){
+                state_outcome_data<-arrange(state_data,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,Hospital.Name) #sorted
+                return(state_outcome_data[1,2])
         }
-        else if (outcome=="heart failure"){
-               data[,17]<-as.numeric(data[,17])
-        }
-        else if (outcome=="pneumonia"){
-                data[,23]<-as.numeric(data[,23])
+        else if(outcome=="heart failure"){
+                state_outcome_data<-arrange(state_data,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)
+                return(state_outcome_data[1,2])
         }
 
-        #find the lowest value in outcome data and returns the hospital index
-
-
-
-        ##return hospital name in the state with lowest 30-day death rate
-
+        else if(outcome=="pneumonia"){
+                state_outcome_data<-arrange(state_data,Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+                return(state_outcome_data[1,2])
+        }
 }
